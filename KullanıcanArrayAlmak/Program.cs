58b20cbe414d677a15d06ed16520e2c1e3e85832
Blau001 <127503550+Blau001@users.noreply.github.com>
2024-08-03 16:44:02 +0300

    KullanıcıdanArrayAlmak

---
+++ KullanıcanArrayAlmak/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KullanıcanArrayAlmak
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sayilar = new int[4];
+
+           
+            for ( int i=0; i<sayilar.Length; i++)
+            {
+                Console.Write("Sayılar dizisinin" + (i + 1) + ". elemanını giriniz: ");
+                sayilar[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("************************************");
+
+            for (int i=0; i<sayilar.Length; i++)
+            {
+                Console.WriteLine(sayilar[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
